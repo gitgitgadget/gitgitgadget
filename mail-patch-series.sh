e245c17d0ac2e66f426a0f20e832e2b6843f9cf7
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-10-23 17:22:39 +0200

    Add a command to submit patch series to upstream Git
    Upstream Git wants to have code contributions as mails. This gets a
    little tedious when the contributions have to be re-worked and
    re-submitted.
    
    Let's have a helper that makes that substantially less tedious.
    
    To avoid the problem noticed by Brian M. Carlson where dash would
    transmogrify the '\n' into a newline in
    
    	x="$(echo "printf '\\n'")"
    	echo "$x"
    
    we force the script to be run through Bash.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ mail-patch-series.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+die () {
+	echo "$*" >&2
+	exit 1
+}
+
+test -n "$(git config alias.send-mbox)" ||
+die "Need an 'send-mbox' alias"
+
+redo=
+while test $# -gt 0
+do
+	case "$1" in
+	--redo)
+		redo=t
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
+test $# = 0 ||
+die "Usage: $0 [--redo]"
+
+# For now, only the Git and Cygwin projects are supported
+if git rev-parse --verify e83c5163316f89bfbde >/dev/null
+then
+	# Git
+	to="--to=\"Junio C Hamano <gitster@pobox.com>\""
+	cc="--cc=git@vger.kernel.org"
+	upstreambranch=junio/next
+elif git rev-parse --verify a3acbf46947e52ff596 >/dev/null
+then
+	# Cygwin
+	to="--to=cygwin-patches@cygwin.com"
+	cc=
+	upstreambranch=cygwin/master
+else
+	die "Unrecognized project"
+fi
+
+# figure out the iteration of this patch series
+branchname="$(git rev-parse --symbolic-full-name HEAD)" &&
+shortname=${branchname#refs/heads/} &&
+test "a$branchname" != "a$shortname" ||
+die "Not on a branch? $branchname"
+
+test -z "$(git rev-list $branchname..$upstreambranch)" ||
+die "Branch $shortname is not rebased to $upstreambranch"
+
+# Cc: from config
+cc="$cc $(git config --get-all branch.$shortname.cc |
+	sed 's/.*/--cc="&"/')" ||
+die "Could not get Cc: list from config"
+
+latesttag="$(git for-each-ref --format='%(refname)' \
+		--sort=-taggerdate refs/tags/"$shortname-v*[0-9]" |
+	sed -n $(echo "$redo" | wc -c)p)"
+if test -z "$latesttag"
+then
+	patch_no=1
+	subject_prefix=
+	in_reply_to=
+	interdiff=
+else
+	test -n "$(git rev-list $branchname...$latesttag)" ||
+	die "Branch $shortname was already submitted: $latesttag"
+
+	patch_no=$((${latesttag##*-v}+1))
+	subject_prefix="--subject-prefix=\"PATCH v$patch_no\""
+	in_reply_to="$(git cat-file tag "$latesttag" |
+		tac | sed '/^$/q' |
+		sed -n 's|.*http://mid.gmane.org/|--in-reply-to=|p')"
+
+	if test -z "$(git rev-list $latesttag..$upstreambranch)"
+	then
+		interdiff="$(git diff $latesttag..$branchname)"
+	else
+		rebasedtag=$latesttag-rebased
+		if git rev-parse --verify $rebasedtag >/dev/null 2>&1
+		then
+			if test -n "$(git rev-list \
+				$rebasedtag..$upstreambranch)"
+			then
+				echo "Re-rebasing $rebasedtag" >&2
+				git checkout $rebasedtag^0 &&
+				git rebase $upstreambranch &&
+				git tag -f -a ${rebasedtag#refs/tags/} &&
+				git checkout $shortname ||
+				die "Could not re-rebase $rebasedtag"
+			fi
+		else
+			# Need rebasing
+			echo "Rebasing $latesttag" >&2
+			git checkout $latesttag^0 &&
+			git rebase $upstreambranch &&
+			git cat-file tag $latesttag |
+			sed '1,/^$/d' |
+			git tag -F - -a ${rebasedtag#refs/tags/} &&
+			git checkout $shortname ||
+			die "Could not re-rebase $rebasedtag"
+		fi
+		interdiff="$(git diff $rebasedtag..$branchname)"
+	fi ||
+	die "Could not generate interdiff"
+fi
+
+# Auto-detect whether we need a cover letter
+cover_letter=
+if test -n "$(git config branch.$shortname.description)"
+then
+	cover_letter=--cover-letter
+elif test 1 -lt $(git rev-list --count $upstreambranch..$branchname)
+then
+	die "Branch $shortname needs a description"
+fi
+
+mbox="$(eval git format-patch $subject_prefix $in_reply_to \
+	$cover_letter $to $cc \
+	--add-header='"Fcc: Sent"' --thread --stdout \
+	$upstreambranch..$branchname)" ||
+die "Could not generate mailbox"
+
+# Add Cc: and explict From: lines for different authors, if needed
+thisauthor="$(git var -l | sed -n 's/^GIT_AUTHOR_IDENT=\([^>]*>\).*/\1/p')"
+otherauthors="$(git log -s --format='%an <%ae>' $upstreambranch..$branchname |
+	grep -v "^$thisauthor$")"
+if test -n "$otherauthors"
+then
+	test -z "$(git log -s --format=%b $upstreambranch..$branchname |
+		grep '^From: .*>$')" ||
+	die "Bogus From: line in commit message"
+
+	mbox="$(echo "$mbox" | sed -e '
+	 /^From: .*>$/{
+	   /^From: '"$thisauthor"'$/b
+
+	   :1
+	   N
+	   /[^\n]$/b1
+
+	   /\nCc: /b2
+
+	   s/^\(From: \([^>]*\).*\)\n/\1\nCc: \2\n\nFrom: \2\n/
+	   b
+
+	   :2
+	   s/^\(From: \([^>]*>\).*\nCc: \)\(.*\)/\1\2, \3\nFrom: \2\n/
+	  }')"
+fi
+
+# Fix Subject line
+test -z "$cover_letter" ||
+mbox="$(echo "$mbox" | sed -e \
+	'/^Subject:/{:1;N;/[^\n]$/b1;N;N;N;N;s/^\([^]]*\] \)\*\*\* [^\n]*\(.*\)\n\n\*\*\*[^\n]*\n\n\(.*\)\n$/\1\3\2\n/;:2;n;b2}')" ||
+die "Could not post-process cover letter"
+
+# tag
+if test -z "$cover_letter"
+then
+	tagmessage="$(git cat-file commit $branchname |
+		sed '1,/^$/d')"
+else
+	tagmessage="$(echo "$mbox" |
+		sed -n -e 's/^Subject: [^]]*] //p' \
+			 -e '/^$/{:1;p;n;/./b1;p;n;/^-- $/q;b1}')"
+fi
+printf "%s\n\nSubmitted-As: http://mid.gmane.org/%s\n%s" \
+	"$tagmessage" \
+	"$(echo "$mbox" | sed -n \
+		'/^Message-Id: /{s/[^:]*: <\(.*\)>/\1/p;q}')" \
+	"$(echo "$in_reply_to" | tr ' ' '\n' | sed -n \
+		's|--in-reply-to=|In-Reply-To: http://mid.gmane.org/|p')" |
+git tag -F - $(test -z "$redo" || echo "-f") -a \
+	"$shortname-v$patch_no" ||
+die "Could not tag $shortname-v$patch_no"
+
+# Insert interdiff
+test -z "$interdiff" ||
+mbox="$(echo "$mbox" |
+	sed '/^-- $/{i'"Interdiff vs v$(($patch_no-1)):"'\
+\
+'"$(echo "$interdiff" | sed -e 's/\\/&&/g' -e 's/$/\\/')"'
+
+:1;n;b1}')"
+
+# Automatically add to drafts
+echo "$mbox" | git send-mbox
