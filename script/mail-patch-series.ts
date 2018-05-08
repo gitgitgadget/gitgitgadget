252ed43ca306431f136ec90f06171646880d9b27
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-05-07 20:50:16 -0400

    Refactor out the Git-related functions into their own module
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- script/mail-patch-series.ts
+++ script/mail-patch-series.ts
@@ -63,25 +63,7 @@
  * `--redo` option.
  */
 
-import { GitProcess, IGitResult, IGitExecutionOptions } from "dugite";
-
-// For convenience, let's add two helpers to call Git:
-
-async function git(args: string[], options?: IGitExecutionOptions | undefined): Promise<string> {
-	let result = await GitProcess.exec(args, '.', options);
-	if (result.exitCode)
-		throw new Error('git ' + args.join(' ') + ' failed with exit code ' + result.exitCode);
-	return result.stdout;
-}
-
-async function gitConfig(key: string): Promise<string> {
-	return (await GitProcess.exec(['config', key], '.')).stdout;
-}
-
-async function gitConfigForEach(key: string, callbackfn: (value: string) => void): Promise<void> {
-	let result = await GitProcess.exec(['config', '--get-all', key], '.');
-	result.stdout.split('\n').map(callbackfn);
-}
+import { git, gitConfig, gitConfigForEach } from '../lib/git';
 
 var publishtoremote: string;
 
