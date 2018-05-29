1a768a50c9e71e89bc7eba4abb396841dbef2fb5
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-05-29 15:47:14 +0200

    Add a function to help with sending mails
    This is a relatively simple, type-safe wrapper around nodemailer,
    crafted specifically to support GitGitGadget's needs.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ lib/send-mail.ts
@@ -0,0 +1,35 @@
+import { createTransport, SendMailOptions } from "nodemailer";
+
+export function sendMail(to: string, cc: string | undefined,
+                         subject: string, body: string,
+                         smtpHost: string, smtpUser: string, smtpPass: string,
+                         from?: string, date?: string, messageId?: string):
+                         Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+        const transporter = createTransport({
+            auth: {
+                pass: smtpPass,
+                user: smtpUser,
+            },
+            host: smtpHost,
+            secure: true,
+        });
+
+        // setup email data with unicode symbols
+        const mailOptions: SendMailOptions = {
+            date,
+            from,
+            subject,
+            text: body,
+            to,
+        };
+
+        transporter.sendMail(mailOptions, (error, info): void => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(info.messageId);
+            }
+        });
+    });
+}
