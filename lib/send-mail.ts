8715b353344a5af57b494a6680d51c5e836b7c9a
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-06-06 10:27:03 +0200

    Offer to send MBox-formatted mails
    This adds a function to parse MBox-formatted mails, in preparation for
    sending it with sendEmail().
    
    This functionality is needed because `git format-patch` outputs
    MBox-formatted mails.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/send-mail.ts
+++ lib/send-mail.ts
@@ -1,27 +1,119 @@
 import { createTransport, SendMailOptions } from "nodemailer";
 
-export function sendMail(to: string, cc: string | undefined,
-                         subject: string, body: string,
-                         smtpHost: string, smtpUser: string, smtpPass: string,
-                         from?: string, date?: string, messageId?: string):
+export interface IParsedMBox {
+    body: string;
+    cc?: string[];
+    date?: string;
+    from: string;
+    headers?: Array<{ key: string; value: string; }>;
+    messageId?: string;
+    subject: string;
+    to: string;
+}
+
+export interface ISMTPOptions {
+    smtpUser: string;
+    smtpHost: string;
+    smtpPass: string;
+}
+
+export async function parseHeadersAndSendMail(mbox: string,
+                                              smtpOptions: ISMTPOptions):
+    Promise<string> {
+    return await sendMail(await parseMBox(mbox), smtpOptions);
+}
+
+function replaceAll(input: string, pattern: string, replacement: string):
+    string {
+    return input.split(pattern).join(replacement);
+}
+
+/**
+ * Parses a mail in mbox format, in preparation for sending it.
+ *
+ * Note: this function does *not* validate the input. For example, it does not
+ * error out if, say, duplicate `Date:` headers were provided.
+ *
+ * @param {string} mbox The mail, in mbox format
+ * @returns {IParsedMBox} the parsed headers/body
+ */
+export async function parseMBox(mbox: string): Promise<IParsedMBox> {
+    const headerEnd = mbox.indexOf("\n\n");
+    if (headerEnd < 0) {
+        throw new Error(`Could not parse mail`);
+    }
+    const headerStart = mbox.startsWith("From ") ? mbox.indexOf("\n") + 1 : 0;
+
+    const header = mbox.substr(headerStart, headerEnd - headerStart);
+    const body = mbox.substr(headerEnd + 2);
+
+    let cc: string[] | undefined;
+    let date: string | undefined;
+    let from: string | undefined;
+    const headers = new Array<{ key: string, value: string }>();
+    let messageId: string | undefined;
+    let subject: string | undefined;
+    let to: string | undefined;
+
+    for (const line of header.split(/\n(?! )/)) {
+        const colon = line.indexOf(": ");
+        if (colon < 0) {
+            throw new Error(`Failed to parse header line '${line}`);
+        }
+        const key = line.substr(0, colon);
+        const value = replaceAll(line.substr(colon + 2), "\n ", " ");
+        switch (key.toLowerCase()) {
+            case "cc": cc = value.split(", "); break;
+            case "date": date = value; break;
+            case "fcc": break;
+            case "from": from = value; break;
+            case "message-id": messageId = value; break;
+            case "subject": subject = value; break;
+            case "to": to = value; break;
+            default:
+                headers.push({ key, value });
+        }
+    }
+
+    if (!to || !subject || !from) {
+        throw new Error(`Missing To, Subject and/or From header:\n${header}`);
+    }
+
+    return {
+        body,
+        cc,
+        date,
+        from,
+        headers,
+        messageId,
+        subject,
+        to,
+    };
+}
+
+export async function sendMail(mail: IParsedMBox,
+                               smtpOptions: ISMTPOptions):
     Promise<string> {
     return new Promise<string>((resolve, reject) => {
         const transporter = createTransport({
             auth: {
-                pass: smtpPass,
-                user: smtpUser,
+                pass: smtpOptions.smtpPass,
+                user: smtpOptions.smtpUser,
             },
-            host: smtpHost,
+            host: smtpOptions.smtpHost,
             secure: true,
         });
 
         // setup email data with unicode symbols
         const mailOptions: SendMailOptions = {
-            date,
-            from,
-            subject,
-            text: body,
-            to,
+            cc: mail.cc || [],
+            date: mail.date,
+            from: mail.from,
+            headers: mail.headers,
+            messageId: mail.messageId,
+            subject: mail.subject,
+            text: mail.body,
+            to: mail.to,
         };
 
         transporter.sendMail(mailOptions, (error, info): void => {
---
+++ tests/send-mail.test.ts
@@ -0,0 +1,57 @@
+import { IParsedMBox, parseMBox } from "../lib/send-mail";
+
+const mbox =
+    `From 566155e00ab72541ff0ac21eab84d087b0e882a5 Mon Sep 17 00:00:00 2001
+Message-Id: <pull.12345.v17.git.gitgitgadget@example.com>
+From: GitGitGadget <gitgitgadget@example.com>
+Date: Fri Sep 21 12:34:56 2001
+Subject: [PATCH 0/3] My first Pull Request!
+Fcc: Sent
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+MIME-Version: 1.0
+To: reviewer@example.com
+Cc: Some Body <somebody@example.com>,
+ And Somebody Else <somebody@else.org>
+
+This Pull Request contains some really important changes that I would love to
+have included in git.git.
+
+Contributor (1):
+  B
+
+Developer (1):
+  C
+
+GitGitGadget (1):
+  A
+
+ A.t | 1 +
+ B.t | 1 +
+ C.t | 1 +
+ 3 files changed, 3 insertions(+)
+ create mode 100644 A.t
+ create mode 100644 B.t
+ create mode 100644 C.t
+
+
+base-commit: 0ae4d8d45ce43d7ad56faff2feeacf8ed5293518
+--${" "}
+2.17.0.windows.1
+`;
+
+test("parse mbox", async () => {
+    const parsed = await parseMBox(mbox);
+    expect(parsed.from).toEqual("GitGitGadget <gitgitgadget@example.com>");
+    expect(parsed.cc).toEqual([
+        "Some Body <somebody@example.com>",
+        "And Somebody Else <somebody@else.org>",
+    ]);
+    expect(parsed.subject).toEqual("[PATCH 0/3] My first Pull Request!");
+    expect(parsed.headers).toEqual([
+        { key: "Content-Type", value: "text/plain; charset=UTF-8" },
+        { key: "Content-Transfer-Encoding", value: "8bit" },
+        { key: "MIME-Version", value: "1.0" },
+    ]);
+    expect(parsed.to).toEqual("reviewer@example.com");
+});
