ad974688baf8ed92b736674e230fc3178b6d0063
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-07-05 11:32:52 +0200

    CIHelper: detect into which upstream branch a PR was merged, if any
    This information requires quite a bit to be detected before that: which
    original commits were already integrated into git.git and what is the
    new SHA-1 of the upstream commits.
    
    While at it, we can also detect which name Junio Hamano (the git.git
    maintainer) chose for the patch series, and we can also update the
    GitHub PR's labels to reflect which upstream branch(es) integrated the
    patch series already.
    
    And we can also close the PR once the patch series hit `master` or
    `maint`.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/ci-helper.ts
+++ lib/ci-helper.ts
@@ -1,8 +1,10 @@
-import { git } from "./git";
+import { commitExists, git } from "./git";
 import { GitNotes } from "./git-notes";
+import { GitGitGadget, IGitGitGadgetOptions } from "./gitgitgadget";
 import { GitHubGlue } from "./github-glue";
 import { MailCommitMapping } from "./mail-commit-mapping";
 import { IMailMetadata } from "./mail-metadata";
+import { IPatchSeriesMetadata } from "./patch-series-metadata";
 
 /*
  * This class offers functions to support the operations we want to perform from
@@ -86,6 +88,180 @@ export class CIHelper {
         return true;
     }
 
+    /**
+     * Process all open PRs.
+     *
+     * @returns true if `refs/notes/gitgitgadget` was updated (and needs to
+     * be pushed)
+     */
+    public async handleOpenPRs(): Promise<boolean> {
+        const options = await this.getGitGitGadgetOptions();
+        if (!options) {
+            throw new Error(`There were no GitGitGadget options to be found?`);
+        }
+        if (!options.openPRs) {
+            return false;
+        }
+        let result: boolean = false;
+        let optionsUpdated: boolean = false;
+        for (const pullRequestURL in options.openPRs) {
+            if (!options.openPRs.hasOwnProperty(pullRequestURL)) {
+                continue;
+            }
+            console.log(`Handling ${pullRequestURL}`);
+            const [notesUpdated, optionsUpdated2] =
+                await this.handlePR(pullRequestURL, options);
+            if (notesUpdated) {
+                result = true;
+            }
+            if (optionsUpdated2) {
+                optionsUpdated = true;
+            }
+        }
+
+        if (optionsUpdated) {
+            await this.notes.set("", options, true);
+            result = true;
+        }
+
+        return result;
+    }
+
+    /**
+     * Handles one PR, i.e. looks whether an upstream commit has been
+     * created/updated that corresponds to the tip commit of the PR, whether it
+     * got its own branch in gitster/git, whether it has been integrated into
+     * any upstream branch, whether it was kicked out of a branch, etc, and
+     * updates the PR on GitHub accordingly (labels, add a comment to inform the
+     * user, close the PR, etc).
+     *
+     * @param {string} pullRequestURL the PR to handle
+     * @param {IGitGitGadgetOptions} options the GitGitGadget options which may
+     * need to be updated.
+     *
+     * @returns two booleans; the first is `true` if there were updates that
+     * require `refs/notes/gitgitgadget` to be pushed. The second is `true`
+     * if the `options` were updated.
+     */
+    public async handlePR(pullRequestURL: string,
+                          options: IGitGitGadgetOptions):
+        Promise<[boolean, boolean]> {
+        await this.maybeUpdateGGGNotes();
+        await this.maybeUpdateMail2CommitMap();
+
+        const prMeta =
+            await this.notes.get<IPatchSeriesMetadata>(pullRequestURL);
+        if (!prMeta || !prMeta.coverLetterMessageId) {
+            return [false, false];
+        }
+
+        const tipCommitInGitGit =
+            await this.identifyUpstreamCommit(prMeta.headCommit);
+        if (!tipCommitInGitGit) {
+            return [false, false];
+        }
+
+        let notesUpdated = false;
+        if (tipCommitInGitGit !== prMeta.tipCommitInGitGit) {
+            prMeta.tipCommitInGitGit = tipCommitInGitGit;
+            notesUpdated = true;
+        }
+
+        // Identify branch in gitster/git
+        let gitsterBranch: string | undefined =
+            await git([
+                "for-each-ref",
+                `--points-at=${tipCommitInGitGit}`,
+                "--format=%(refname)",
+                "refs/remotes/gitster/",
+            ], { workDir: this.workDir });
+        if (gitsterBranch) {
+            const newline = gitsterBranch.indexOf("\n");
+            if (newline > 0) {
+                const comment2 = `Found multiple candidates in gitster/git:\n${
+                    gitsterBranch};\n\nUsing the first one.`;
+                const url2 =
+                    await this.github.addPRComment(pullRequestURL, comment2);
+                console.log(`Added comment about ${gitsterBranch}: ${url2}`);
+
+                gitsterBranch = gitsterBranch.substr(0, newline);
+            }
+            gitsterBranch =
+                gitsterBranch.replace(/^refs\/remotes\/gitster\//, "");
+
+            const comment = `This branch is now known as [\`${gitsterBranch
+                }\`](https://github.com/gitster/git/commits/${gitsterBranch}).`;
+            if (prMeta.branchNameInGitsterGit !== gitsterBranch) {
+                prMeta.branchNameInGitsterGit = gitsterBranch;
+                notesUpdated = true;
+
+                const url =
+                    await this.github.addPRComment(pullRequestURL, comment);
+                console.log(`Added comment about ${gitsterBranch}: ${url}`);
+            }
+        }
+
+        let closePR: string | undefined;
+        const prLabelsToAdd = [];
+        for (const branch of ["pu", "next", "master", "maint"]) {
+            const mergeCommit =
+                await this.identifyMergeCommit(branch, tipCommitInGitGit);
+            if (!mergeCommit) {
+                continue;
+            }
+
+            if (branch === "master" || branch === "maint") {
+                closePR = mergeCommit;
+            }
+
+            if (!prMeta.mergedIntoUpstream) {
+                prMeta.mergedIntoUpstream = {};
+            }
+            if (prMeta.mergedIntoUpstream[branch] !== mergeCommit) {
+                prMeta.mergedIntoUpstream[branch] = mergeCommit;
+                notesUpdated = true;
+
+                // Add label on GitHub
+                prLabelsToAdd.push(branch);
+
+                // Add comment on GitHub
+                const comment = `This patch series was integrated into ${branch
+                    } via https://github.com/git/git/commit/${mergeCommit}.`;
+                const url =
+                    await this.github.addPRComment(pullRequestURL, comment);
+                console.log(`Added comment about ${branch}: ${url}`);
+            }
+        }
+
+        if (prLabelsToAdd.length) {
+            await this.github.setPRLabels(pullRequestURL, prLabelsToAdd);
+        }
+
+        let optionsUpdated = false;
+        if (closePR) {
+            if (options.openPRs) {
+                delete options.openPRs[pullRequestURL];
+                optionsUpdated = true;
+            }
+            // Remove items from activeMessageIDs
+            if (options.activeMessageIDs) {
+                for (const rev of await this.getOriginalCommitsForPR(prMeta)) {
+                    const messageID = await this.notes.getLastCommitNote(rev);
+                    delete options.activeMessageIDs[messageID];
+                }
+                optionsUpdated = true;
+            }
+
+            await this.github.closePR(pullRequestURL, closePR);
+        }
+
+        if (notesUpdated) {
+            await this.notes.set(pullRequestURL, prMeta, true);
+        }
+
+        return [notesUpdated, optionsUpdated];
+    }
+
     public async getMessageIdForOriginalCommit(commit: string):
         Promise<string | undefined> {
         await this.maybeUpdateGGGNotes();
@@ -145,6 +321,45 @@ export class CIHelper {
                 return commit;
             }
         }
+    }
+
+    public async getGitGitGadgetOptions(): Promise<IGitGitGadgetOptions> {
+        await this.maybeUpdateGGGNotes();
+        const options = await this.notes.get<IGitGitGadgetOptions>("");
+        if (options === undefined) {
+            throw new Error("No GitGitGadgetOptions?!?!?");
+        }
+        return options;
+    }
+
+    public async getOriginalCommitsForPR(prMeta: IPatchSeriesMetadata):
+        Promise<string[]> {
+        if (!this.workDir) {
+            throw new Error(`Need a workDir`);
+        }
+        if (!await commitExists(prMeta.headCommit, this.workDir)) {
+            if (!prMeta.pullRequestURL) {
+                throw new Error(`Require URL in ${
+                    JSON.stringify(prMeta, null, 4)}`);
+            }
+            if (!prMeta.latestTag) {
+                throw new Error(`Cannot fetch commits without tag`);
+            }
+            const [owner, repo, nr] =
+                GitGitGadget.parsePullRequestURL(prMeta.pullRequestURL);
+            const fetchURL = `https://github.com/${owner}/${repo}`;
+            const fetchRef = `refs/pull/${nr}/head`;
+            await git(["fetch", fetchURL, fetchRef, prMeta.latestTag], {
+                workDir: this.workDir,
+            });
+        }
+        const revs = await git([
+            "rev-list",
+            `${prMeta.baseCommit}..${prMeta.headCommit}`,
+        ], {
+                workDir: this.workDir,
+            });
+        return revs.split(/\s+/);
     }
 
     private async maybeUpdateGGGNotes(): Promise<void> {
