198ba22827b61792794f02a41a8d6c05788c79c4
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-01-25 14:24:38 +0100

    IMailMetadata: fix the data type of the comment ID
    It really is a number, not a string.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/mail-metadata.ts
+++ lib/mail-metadata.ts
@@ -1,7 +1,7 @@
 export interface IMailMetadata {
     readonly messageID: string;
     pullRequestURL?: string;
-    issueCommentId?: string;
+    issueCommentId?: number;
     originalCommit?: string;
     commitInGitGit?: string;
 }
