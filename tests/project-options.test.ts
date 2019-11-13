14d4504e3d51a616247b07f2f5f98529bf6acb08
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-11-13 12:04:14 +0100

    Remove unnecessary trailing comma
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- tests/project-options.test.ts
+++ tests/project-options.test.ts
@@ -3,7 +3,7 @@ import { isDirectory } from "../lib/fs-util";
 import { GitNotes } from "../lib/git-notes";
 import { PatchSeries } from "../lib/patch-series";
 import { ProjectOptions } from "../lib/project-options";
-import { testCreateRepo, } from "./test-lib";
+import { testCreateRepo } from "./test-lib";
 
 // This test script might take quite a while to run
 jest.setTimeout(20000);
