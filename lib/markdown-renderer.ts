ee937a27a0e08d0a97e12df27c55be6c097947e1
Chris. Webster <chris@webstech.net>
2020-06-09 15:51:32 -0700

    Update @types/html-to-text to 5.1.1
    The types are updated to support the format attribute so
    a local instance of HtmlToTextOptions is now used.
    
    Signed-off-by: Chris. Webster <chris@webstech.net>

--- lib/markdown-renderer.ts
+++ lib/markdown-renderer.ts
@@ -1,41 +1,33 @@
-import { fromString } from "html-to-text";
+import { fromString, HtmlToTextOptions } from "html-to-text";
 import marked from "marked";
 
-// @types/html-to-text v1.4.31 does not handle the `format` attribute; To work
-// around that, we simply implement a class that has that (we want a different
-// rendering of headings than html-to-text provides by default).
+// Provide our own renderings of headings and blockquotes.
 
-class MyOptions implements HtmlToTextOptions {
-    public hideLinkHrefIfSameAsText = true;
-    public uppercaseHeadings = false;
-    public wordwrap = 76;
+export function md2text(markdown: string, columns = 76): string {
+    const formatOptions: HtmlToTextOptions = {
+        hideLinkHrefIfSameAsText: true,
+        uppercaseHeadings: false,
+        wordwrap: columns,
 
-    public format = {
-        heading(elem: any, fn: any, options: any): string {
-            const heading: string = fn(elem.children, options);
-            const underline = heading.substr(heading.lastIndexOf("\n") + 1)
-                .replace(/./g, "=");
-            return `${heading}\n${underline}\n\n`;
-        },
-        blockquote(elem: any, fn: any, options: any): string {
-            const indentOptions = Object.assign({}, options);
-            // decrease word wrap, but only to a minimum of 20 columns/line
-            indentOptions.wordwrap = Math.max(20, options.wordwrap - 2);
+        format: {
+            heading: (elem, fn, options) => {
+                const heading = fn(elem.children, options);
+                const underline = heading.substr(heading.lastIndexOf("\n") + 1)
+                    .replace(/./g, "=");
+                return `${heading}\n${underline}\n\n`;
+            },
+            blockquote: (elem, fn, options) => {
+                const indentOptions = Object.assign({ wordwrap: 76 }, options);
+                // decrease word wrap, but only to a minimum of 20 columns/line
+                indentOptions.wordwrap = Math.max(20, indentOptions.wordwrap-2);
 
-            const block: string = fn(elem.children, indentOptions);
-            return block.replace(/^>/mg, ">>") // add to quote
-                .replace(/^(?!>|$)/mg, "> ")   // new quote
-                .replace(/(^|\n)(\n)(?!$)/g, "$1>$2"); // quote empty lines
+                const block = fn(elem.children, indentOptions);
+                return block.replace(/^>/mg, ">>") // add to quote
+                    .replace(/^(?!>|$)/mg, "> ")   // new quote
+                    .replace(/(^|\n)(\n)(?!$)/g, "$1>$2"); // quote empty lines
+            },
         },
     };
 
-    public constructor(columns?: number) {
-        if (columns !== undefined) {
-            this.wordwrap = columns;
-        }
-    }
-}
-
-export function md2text(markdown: string, columns?: number): string {
-    return fromString(marked(markdown), new MyOptions(columns));
+    return fromString(marked(markdown), formatOptions);
 }
