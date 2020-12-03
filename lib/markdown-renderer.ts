0a7077fc03b5c6ddf65be3ccc39e66189a47c963
Chris. Webster <chris@webstech.net>
2020-12-03 11:41:06 -0800

    Support html-to-text v6
    Support new formatters and options changes.
    
    Add block quote tests to validate wrap points.
    
    Signed-off-by: Chris. Webster <chris@webstech.net>

--- lib/markdown-renderer.ts
+++ lib/markdown-renderer.ts
@@ -1,33 +1,62 @@
-import { fromString, HtmlToTextOptions } from "html-to-text";
+import { htmlToText, HtmlToTextOptions } from "html-to-text";
 import marked from "marked";
 
 // Provide our own renderings of headings and block quotes.
 
 export function md2text(markdown: string, columns = 76): string {
+
+    const headerOptions =  {
+                options: {
+                    uppercase: false
+                },
+                format: "headerFormatter",
+            };
+
+    let quoteDepth = 0;
     const formatOptions: HtmlToTextOptions = {
-        hideLinkHrefIfSameAsText: true,
-        uppercaseHeadings: false,
         wordwrap: columns,
-
-        format: {
-            heading: (elem: { children: any[] }, fn, options) => {
-                const heading = fn(elem.children, options);
-                const underline = heading.substr(heading.lastIndexOf("\n") + 1)
-                    .replace(/./g, "=");
-                return `${heading}\n${underline}\n\n`;
+        formatters: {
+            headerFormatter: (elem, walk, builder, options) => {
+                builder.openBlock(options.leadingLineBreaks || 2);
+                walk(elem.children, builder);
+                builder.closeBlock(options.trailingLineBreaks || 2,
+                    str => {
+                        const underline = str.substr(str.lastIndexOf("\n") + 1)
+                            .replace(/./g, "=");
+                        return `${str}\n${underline}`;
+                    });
             },
-            blockquote: (elem: { children: any[] }, fn, options) => {
-                const indentOptions = Object.assign({ wordwrap: 76 }, options);
-                // decrease word wrap, but only to a minimum of 20 columns/line
-                indentOptions.wordwrap = Math.max(20, indentOptions.wordwrap-2);
-
-                const block = fn(elem.children, indentOptions);
-                return block.replace(/^>/mg, ">>") // add to quote
-                    .replace(/^(?!>|$)/mg, "> ")   // new quote
-                    .replace(/(^|\n)(\n)(?!$)/g, "$1>$2"); // quote empty lines
+            blockFormatter: (elem, walk, builder, options) => {
+                builder.openBlock(options.leadingLineBreaks || 2,
+                    quoteDepth? 1 : 2);
+                quoteDepth++;
+                walk(elem.children, builder);
+                quoteDepth--;
+                builder.closeBlock(options.trailingLineBreaks || 2,
+                    str => { return str
+                        .replace(/^>/mg, ">>") // add to quote
+                        .replace(/^(?!>|$)/mg, "> ")   // new quote
+                        .replace(/(^|\n)(\n)(?!$)/g, "$1>$2") // quote empty
+                    });
+            },
+        },
+        tags: {
+            a: {
+                options: {
+                    hideLinkHrefIfSameAsText: true,
+                },
+            },
+            h1: headerOptions,
+            h2: headerOptions,
+            h3: headerOptions,
+            blockquote: {
+                options: {
+                    trimEmptyLines: false
+                },
+                format: "blockFormatter",
             },
         },
     };
 
-    return fromString(marked(markdown), formatOptions);
+    return htmlToText(marked(markdown), formatOptions);
 }
