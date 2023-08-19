793d93342e221eb1b953b1692264dd3fc2fe6445
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-08-19 18:09:29 +0200

    Add the `gitgitgadget/init` GitHub Action
    The idea is to let `ncc` turn the CIHelper code into a dependency-free
    `dist/index.js` and then remove all files other than `*/action.yml` and
    `*/index.js`, commit and tag that. That's the GitHub Action, ready to
    be used in GitHub workflows.
    
    Since `ncc` produces an ECMAScript module, we cannot use the old-style
    `require()` call but need to use `await import()` instead.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ init/index.js
@@ -0,0 +1,41 @@
+async function run() {
+    const { execSync } = await import('child_process')
+    const { CIHelper } = await import('../dist/index.js')
+
+    const core = CIHelper.getActionsCore()
+
+    // help dugite realize where `git` is...
+    process.env.LOCAL_GIT_DIRECTORY = '/usr/'
+
+    execSync('git init --bare --initial-branch main')
+    const ci = new CIHelper('.', await CIHelper.getConfig(), true)
+
+    const gggAppID = core.getInput('gitgitgadget-app-id')
+    const gggPrivateKey = core.getInput('gitgitgadget-private-key')
+    const gggAppID2 = core.getInput('gitgitgadget-git-app-id')
+    const gggPrivateKey2 = core.getInput('gitgitgadget-git-private-key')
+
+    for (const options of [
+        {
+            name: 'gitgitgadget',
+            appID: gggAppID,
+            privateKey: gggPrivateKey
+        },
+        {
+            name: 'git',
+            appID: gggAppID2,
+            privateKey: gggPrivateKey2
+        },
+        {
+            name: 'dscho',
+            appID: gggAppID2,
+            privateKey: gggPrivateKey2
+        }
+    ]) {
+        const token = await ci.configureGitHubAppToken(options)
+        core.setSecret(token)
+        core.setOutput(`${options.name}-token`, token)
+    }
+}
+
+run()
