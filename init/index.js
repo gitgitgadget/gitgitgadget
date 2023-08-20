313a03898bac14f9a690af94b8e9953c55ab04a3
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-08-20 09:33:16 +0200

    TEST: add a reaction
    This adds a reaction to a PR comment in my fork, to verify that
    GitGitGadget can do such things when called via the new GitHub Action.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- init/index.js
+++ init/index.js
@@ -3,6 +3,18 @@ async function run() {
 
   const ci = new CIHelper()
   await ci.setupGitHubAction()
+
+  // add a reaction
+  const core = CIHelper.getActionsCore()
+  const prCommentUrl = core.getInput("pr-comment-url")
+  const [, owner, repo, prNumber, commentId] = prCommentUrl.match(
+    /^https:\/\/github\.com\/([^/]+)\/([^/]+)\/pull\/(\d+)#issuecomment-(\d+)/,
+  )
+  if (!owner) {
+    throw new Error(`Invalid PR comment URL: ${prCommentUrl}`)
+  }
+  const reaction = core.getInput("reaction")
+  await ci.github.addReaction(owner, repo, commentId, reaction)
 }
 
 run()
