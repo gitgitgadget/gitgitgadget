6d397b0fa4a8ebc5f07c0027ac9d2bf628c6e703
Johannes Schindelin <johannes.schindelin@gmx.de>
2023-12-31 17:40:39 +0000

    Remove support for the CLI mode
    The GitGitGadget project originates from a Unix Shell script
    named `mail-patch-series.sh` which is still available at
    https://github.com/dscho/mail-patch-series. This script was first
    converted to Typescript, and subsequently into a GitHub App whose
    compute-intensive part is performed by Azure Pipelines.
    
    The Typescript variant of the original command-line script is still
    in the code-base, as is the support code only needed by that script
    (such as extracting patch series' metadata from a tag). However,
    this script is no longer used (not even by me, I turned back to the
    Unix Shell variant for use cases not yet supported by GitGitGadget,
    such as contributing to Cygwin).
    
    Therefore, let's get rid of the script as well as the support code
    needed only by that script.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/project-options.ts
+++ lib/project-options.ts
@@ -1,27 +1,8 @@
-import { commitExists, git, gitConfig, gitConfigForEach, revParse } from "./git";
+import { commitExists, git, revParse } from "./git";
 import { IConfig, getConfig, projectInfo } from "./project-config";
 
 // For now, only the Git, Cygwin and BusyBox projects are supported
 export class ProjectOptions {
-    public static async getBranchName(workDir: string): Promise<string> {
-        // Get the current branch name
-        const ref = await git(["rev-parse", "--symbolic-full-name", "HEAD"], { workDir });
-        const match = ref.match(/^refs\/heads\/(.*)/);
-        if (!match) {
-            throw new Error("Not on a branch (" + ref + ")?");
-        }
-        return match[1];
-    }
-
-    public static async getLocal(workDir = "."): Promise<ProjectOptions> {
-        const branchName = await ProjectOptions.getBranchName(workDir);
-        const cc = await ProjectOptions.getCc(branchName, workDir);
-        const publishToRemote = await gitConfig("mail.publishtoremote", workDir);
-        const baseBranch = await ProjectOptions.determineBaseBranch(workDir, branchName, publishToRemote);
-
-        return await ProjectOptions.get(workDir, branchName, cc, baseBranch, publishToRemote);
-    }
-
     public static async get(workDir: string, branchName: string, cc: string[], basedOn?: string,
                             publishToRemote?: string, baseCommit?: string): Promise<ProjectOptions> {
         const config: IConfig = getConfig();
@@ -85,42 +66,6 @@ export class ProjectOptions {
 
         return new ProjectOptions(branchName, upstreamBranch, basedOn, publishToRemote, to, cc, midUrlPrefix,
                                   workDir, baseCommit);
-    }
-
-    protected static async determineBaseBranch(workDir: string, branchName: string, publishToRemote?: string):
-        Promise<string | undefined> {
-        const basedOn = await gitConfig(`branch.${branchName}.basedon`, workDir);
-        if (!basedOn || !await commitExists(basedOn, workDir)) {
-            return undefined;
-        }
-
-        if (!publishToRemote) {
-            throw new Error("Need a remote to publish to");
-        }
-
-        const remoteRef = `refs/remotes/${publishToRemote}/${basedOn}`;
-        if (!await commitExists(remoteRef, workDir)) {
-            throw new Error(`${basedOn} not pushed to ${publishToRemote}`);
-        }
-
-        const commit = await git(["rev-parse", "-q", "--verify", remoteRef], { workDir });
-        if (await git(["rev-parse", basedOn]) !== commit) {
-            throw new Error(`${basedOn} on ${publishToRemote} disagrees with local branch`);
-        }
-
-        return basedOn;
-    }
-
-    protected static async getCc(branchName: string, workDir: string): Promise<string[]> {
-        // Cc: from config
-        const cc: string[] = [];
-        const forEach = (email: string): void => {
-            if (email) {
-                cc.push(email);
-            }
-        };
-        await gitConfigForEach(`branch.${branchName}.cc`, forEach, workDir);
-        return cc;
     }
 
     public readonly branchName: string;
