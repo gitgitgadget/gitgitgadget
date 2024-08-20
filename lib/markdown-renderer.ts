42bce249fd2fd78c5a4c7166e7fbbbec0cbe0d14
Chris. Webster <chris@webstech.net>
2024-08-20 16:07:29 -0700

    lint: line length and space issues
    Also includes some terminator issues.
    
    Signed-off-by: Chris. Webster <chris@webstech.net>

--- lib/markdown-renderer.ts
+++ lib/markdown-renderer.ts
@@ -4,37 +4,38 @@ import { marked } from "marked";
 // Provide our own renderings of headings and block quotes.
 
 export function md2text(markdown: string, columns = 76): string {
-
     let quoteDepth = 0;
     const formatOptions: HtmlToTextOptions = {
         wordwrap: columns,
         formatters: {
             headerFormatter: (elem, walk, builder, options) => {
                 builder.openBlock({
-                    leadingLineBreaks: options.leadingLineBreaks || 2});
+                    leadingLineBreaks: options.leadingLineBreaks || 2 });
                 walk(elem.children, builder);
                 builder.closeBlock({
                     trailingLineBreaks: options.trailingLineBreaks || 2,
-                    blockTransform: str => {
+                    blockTransform: (str) => {
                         const underline = str.substring(str.lastIndexOf("\n") + 1)
                             .replace(/./g, "=");
                         return `${str}\n${underline}`;
-                    }});
+                    },
+                });
             },
             blockFormatter: (elem, walk, builder, options) => {
                 builder.openBlock({
                     leadingLineBreaks: options.leadingLineBreaks || 2,
-                    reservedLineLength: quoteDepth? 1 : 2});
+                    reservedLineLength: quoteDepth ? 1 : 2});
                 quoteDepth++;
                 walk(elem.children, builder);
                 quoteDepth--;
-                builder.closeBlock({ trailingLineBreaks:
-                    options.trailingLineBreaks || 2,
-                    blockTransform: str => { return str
-                        .replace(/^>/mg, ">>") // add to quote
-                        .replace(/^(?!>|$)/mg, "> ")   // new quote
+                builder.closeBlock({
+                    trailingLineBreaks: options.trailingLineBreaks || 2,
+                    blockTransform: (str) => { return str
+                        .replace(/^>/gm, ">>") // add to quote
+                        .replace(/^(?!>|$)/gm, "> ") // new quote
                         .replace(/(^|\n)(\n)(?!$)/g, "$1>$2"); // quote empty
-                    }});
+                    },
+                });
             },
             checkBoxFormatter: (elem, _walk, builder) => {
                 const attribs = elem.attribs as { checked?: string };
@@ -51,34 +52,34 @@ export function md2text(markdown: string, columns = 76): string {
             {
                 selector: "h1",
                 options: {
-                    uppercase: false
+                    uppercase: false,
                 },
                 format: "headerFormatter",
             },
             {
                 selector: "h2",
                 options: {
-                    uppercase: false
+                    uppercase: false,
                 },
                 format: "headerFormatter",
             },
             {
                 selector: "h3",
                 options: {
-                    uppercase: false
+                    uppercase: false,
                 },
                 format: "headerFormatter",
             },
             {
                 selector: "blockquote",
                 options: {
-                    trimEmptyLines: false
+                    trimEmptyLines: false,
                 },
-                format: "blockFormatter"
+                format: "blockFormatter",
             },
             {
                 selector: "input[type=checkbox]",
-                format: "checkBoxFormatter"
+                format: "checkBoxFormatter",
             },
         ],
     };
