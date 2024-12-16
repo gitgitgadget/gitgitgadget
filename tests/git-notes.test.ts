ELSYEBAN999/Elsyeban1

notes: implement a method to merge diverging notes
To handle the situation when concurrent GitGitGadget operations want to
modify the state that is persisted in Git notes (such as
`refs/notes/gitgitgadget`), we need a way to reconcile diverging Git
notes.

Happily, the way GitGitGadget works makes this relatively
straight-forward, as concurrent operations usually do not conflict and
merely need to add/modify/remove independent attributes and/or values.
This allows us to treat these Git notes as kind of a Conflict-free
Replicated Data Type (https://en.wikipedia.org/wiki/CRDT).

The strategy implemented in this patch is relatively simple, and follows
the "local-first" idea (where state is applied locally first and only
then persisted remotely): The newly added method determines what changes
were made locally, and then reapplies them on top of the upstream note
commit. The local operations need to fall into one of the following
categories:

- a note has been appended to

- a single line of text (which may, or may not, be a stringified JSON
  object) is added as a note on an object that had not had a note yet

- a note containing a single line of text has been replaced with a new,
  single line of text

- a note that contained a JSON object (stringified as a single line of
  text) has been modified. Valid modifications include:

  - an attribute that was added

  - an attribute that has been removed

  - an attribute containing a primitive type (string, number or boolean)
    has been modified (if local and upstream diverged, local wins)

  - items from an attribute containing an array have been removed and/or
    added

  - an attribute containing a Plain Old Javascript Object has seen any
    of these modifications

Other modifications are as yet unhandled.

For historical reasons (*cough* Git commands implemented in Unix shell
scripts *cough*) it is impossible to add a Git note that does not end in
a newline character. Let's ensure that we don't do that, either.

To 

tests/git-notes.test.ts
@@ -1,7 +1,7 @@
 import { expect, jest, test } from "@jest/globals";
 import { isDirectory } from "../lib/fs-util";
 import { git, revParse } from "../lib/git";
-import { GitNotes } from "../lib/git-notes";
+import { GitNotes, POJO } from "../lib/git-notes";
 import { IPatchSeriesMetadata } from "../lib/patch-series-metadata";
 import { testCreateRepo } from "./test-lib";
 
@@ -43,4 +43,50 @@ test("set/get notes", async () => {
     expect(await notes.appendCommitNote(commit as string, "2")).toEqual("");
     expect(await notes.getCommitNotes(commit as string)).toEqual("1\n\n2");
     expect(await notes.getLastCommitNote(commit as string)).toEqual("2");
+});
+
+test("notesMerge()", async () => {
+    const repo = await testCreateRepo(__filename);
+    const notes = new GitNotes(repo.workDir);
+
+    const somePrimeNumbers = [2, 3, 7, 11, 13];
+    const o: POJO = { hello: "world", somePrimeNumbers };
+    o.extra = true;
+    const o2 = JSON.parse(JSON.stringify(o)) as POJO; // o.clone()
+
+    await notes.set("", o);
+    const commit = await revParse(notes.notesRef, repo.workDir);
+    expect(commit).not.toBeUndefined();
+    await notes.appendCommitNote(commit as string, "first note");
+
+    // branch off
+    const branchPoint = await revParse(notes.notesRef, repo.workDir);
+    await notes.appendCommitNote(commit as string, "low note");
+    somePrimeNumbers.push(17);
+    o.hello = "World!!!";
+    delete o.extra;
+    await notes.set("", o, true);
+    const branch1 = await revParse(notes.notesRef, repo.workDir);
+
+    // rewind and make some local-first changes
+    await git(["update-ref", notes.notesRef, branchPoint as string, branch1 as string], { workDir: repo.workDir });
+    await notes.appendCommitNote(commit as string, "second note");
+    await notes.setString("notes", "tsforyou");
+    o2.hello = "you!";
+    (o2.somePrimeNumbers as number[]).splice(2, 2);
+    (o2.somePrimeNumbers as number[]).push(29);
+    o2.oh = ["hai", "cat"];
+    await notes.set("", o2, true);
+
+    // now merge branch1 (or: replay HEAD onto branch1)
+    await notes.notesSync(branch1 as string);
+
+    const o3 = await notes.get("") as POJO;
+    expect(o3.extra).toBeUndefined();
+    expect(o3.somePrimeNumbers).toEqual([2, 3, 13, 17, 29]);
+    expect(o3.hello).toEqual("you!");
+    expect(o3.oh).toEqual(["hai", "cat"]);
+
+    const addedNotes = await notes.getCommitNotes(commit as string);
+    expect(addedNotes).toEqual(`first note\n\nlow note\n\nsecond note`);
 });

gitgitgadget/gitgitgadget

tests/git-notes.test.ts
@@ -2,7 +2,7 @@ import { expect, jest, test } from "@jest/globals";
 import { fileURLToPath } from "url";
 import { isDirectory } from "../lib/fs-util.js";
 import { git, revParse } from "../lib/git.js";
-import { GitNotes } from "../lib/git-notes.js";
+import { GitNotes, POJO } from "../lib/git-notes.js";
 import { IPatchSeriesMetadata } from "../lib/patch-series-metadata.js";
 import { testCreateRepo } from "./test-lib.js";
 
@@ -61,4 +61,50 @@ test("set/get notes", async () => {
     await expect(notesNotM2Chead.update(".")).rejects.toThrow(/know how to update/);
     const notesNotM2Ctail = new GitNotes(repo.workDir, "refs/notes/mail-to-commitx");
     await expect(notesNotM2Ctail.update(".")).rejects.toThrow(/know how to update/);
+});
+
+test("notesSync()", async () => {
+    const repo = await testCreateRepo(sourceFileName);
+    const notes = new GitNotes(repo.workDir);
+
+    const somePrimeNumbers = [2, 3, 7, 11, 13];
+    const o: POJO = { hello: "world", somePrimeNumbers };
+    o.extra = true;
+    const o2 = JSON.parse(JSON.stringify(o)) as POJO; // o.clone()
+
+    await notes.set("", o);
+    const commit = await revParse(notes.notesRef, repo.workDir);
+    expect(commit).not.toBeUndefined();
+    await notes.appendCommitNote(commit as string, "first note");
+
+    // branch off
+    const branchPoint = await revParse(notes.notesRef, repo.workDir);
+    await notes.appendCommitNote(commit as string, "low note");
+    somePrimeNumbers.push(17);
+    o.hello = "World!!!";
+    delete o.extra;
+    await notes.set("", o, true);
+    const branch1 = await revParse(notes.notesRef, repo.workDir);
+
+    // rewind and make some local-first changes
+    await git(["update-ref", notes.notesRef, branchPoint as string, branch1 as string], { workDir: repo.workDir });
+    await notes.appendCommitNote(commit as string, "second note");
+    await notes.setString("notes", "tsforyou");
+    o2.hello = "you!";
+    (o2.somePrimeNumbers as number[]).splice(2, 2);
+    (o2.somePrimeNumbers as number[]).push(29);
+    o2.oh = ["hai", "cat"];
+    await notes.set("", o2, true);
+
+    // now merge branch1 (or: replay HEAD onto branch1)
+    await notes.notesSync(branch1 as string);
+
+    const o3 = (await notes.get("")) as POJO;
+    expect(o3.extra).toBeUndefined();
+    expect(o3.somePrimeNumbers).toEqual([2, 3, 13, 17, 29]);
+    expect(o3.hello).toEqual("you!");
+    expect(o3.oh).toEqual(["hai", "cat"]);
+
+    const addedNotes = await notes.getCommitNotes(commit as string);
+    expect(addedNotes).toEqual(`first note\n\nlow note\n\nsecond note`);
 });