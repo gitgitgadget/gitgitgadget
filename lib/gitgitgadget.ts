9864cf3b963d21dcd6d5fb21cb77f9720fa6f442
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-02-08 11:29:46 +0100

    rfc: fix a new lint error
    In aefc723f (correspond draft PR and 'RFC PATCH', 2025-01-16) we
    introduced code that my local linter claims is an unsafe assignment.
    Let's work around that.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/gitgitgadget.ts
+++ lib/gitgitgadget.ts
@@ -260,7 +260,7 @@ export class GitGitGadget {
         const previousTag = metadata && metadata.latestTag ? `refs/tags/${metadata.latestTag}` : undefined;
         // update work repo from base
         await this.updateNotesAndPullRef(pr.baseOwner, pr.number, previousTag);
-        options.rfc = pr.draft;
+        options.rfc = pr.draft ?? false;
 
         const series = await PatchSeries.getFromNotes(
             this.notes,
