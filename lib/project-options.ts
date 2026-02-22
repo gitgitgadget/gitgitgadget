gitgitgadget/gitgitgadget

commit c72d162e4c868b7115c4688f918c82fa8a6eb2bf

project-options: handle shallow worktrees gracefully
To allow for more ephemeral operations, where we will use shallow,
bare, partial clones, let's not insist on the presence of root commits
to determine which project we're on. Instead, use the presence of
certain files or directories in the base commit as better tell-tales.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

lib/project-options.ts
@@ -18,13 +18,12 @@ export class ProjectOptions {
             for (const user of project.cc) {
                 cc.push(user);
             }
-        } else if (await commitExists("cb07fc2a29c86d1bc11", workDir) &&
-            await revParse(`${baseCommit}:git-gui.sh`, workDir) !== undefined) {
+        } else if (await revParse(`${baseCommit}:git-gui.sh`, workDir) !== undefined) {
             // Git GUI
             to = "--to=git@vger.kernel.org";
             cc.push("Pratyush Yadav <me@yadavpratyush.com>");
             upstreamBranch = "git-gui/master";
-        } else if (await commitExists("e83c5163316f89bfbde", workDir)) {
+        } else if (await revParse(`${baseCommit}:git.c`, workDir) !== undefined) {
             // Git
             to = "--to=git@vger.kernel.org";
             // Do *not* Cc: Junio Hamano by default
@@ -36,12 +35,12 @@ export class ProjectOptions {
                 upstreamBranch = "upstream/master";
             }
             midUrlPrefix = "https://lore.kernel.org/git/";
-        } else if (await commitExists("a3acbf46947e52ff596", workDir)) {
+        } else if (await revParse(`${baseCommit}:winsup`, workDir) !== undefined) {
             // Cygwin
             to = "--to=cygwin-patches@cygwin.com";
             upstreamBranch = "cygwin/master";
             midUrlPrefix = "https://www.mail-archive.com/search?l=cygwin-patches@cygwin.com&q=";
-        } else if (await commitExists("cc8ed39b240180b5881", workDir)) {
+        } else if (await revParse(`${baseCommit}:include/busybox.h`, workDir) !== undefined) {
             // BusyBox
             to = "--to=busybox@busybox.net";
             upstreamBranch = "busybox/master";

commit ce738dbfdbd880e71a36421f2e9885e28ca57a6d

Allow contributions to the `todo` branch
The `todo` branch is completely divergent from Git's `master` branch,
and as such contains different files. Yet it still belongs to the Git
project...

This is needed to fix the `Unrecognized project` error in https://
github.com/git/git/pull/2209

The underlying logic was correct until c72d162e (project-options: handle
shallow worktrees gracefully, 2024-01-01) broke it; This change was
needed to allow migrating from running on a self-hosted Azure Pipelines
runner to running on hosted GitHub Actions runners, but the logic was
incomplete and would no longer reliably detect whether GitGitGadget is
running in a PR that targets the Git project.

Another fix would have been to move forward with the `vars.CONFIG` idea
championed in https://github.com/gitgitgadget/gitgitgadget/pull/1991.
However, my attention was directed away from that effort for a long time
now, and it won't be _so_ easy to push that over the finish line. Let's
unblock the contributor first.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

lib/project-options.ts
@@ -29,7 +29,14 @@ export class ProjectOptions {
             to = "--to=git@vger.kernel.org";
             cc.push("Johannes Sixt <j6t@kdbg.org>");
             upstreamBranch = "git-gui/master";
-        } else if ((await revParse(`${baseCommit}:git.c`, workDir)) !== undefined) {
+        } else if (
+            (await revParse(`${baseCommit}:git.c`, workDir)) !== undefined
+            || (
+                // gitster/git's `todo` branch, see https://github.com/git/git/pull/2209
+                (await revParse(`${baseCommit}:README.cooking`, workDir)) !== undefined
+                && (await revParse(`${baseCommit}:jc.png`, workDir)) !== undefined
+            )
+        ) {
             // Git
             to = "--to=git@vger.kernel.org";
             // Do *not* Cc: Junio Hamano by default